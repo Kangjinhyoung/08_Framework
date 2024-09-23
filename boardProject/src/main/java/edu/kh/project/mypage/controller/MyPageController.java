248de8e0475c85387231313eab9221875e77dc3c
Kangjinhyoung <a51615161@daum.net>
2024-09-23 16:45:09 +0900

    20240923_수업내용

--- boardProject/src/main/java/edu/kh/project/mypage/controller/MyPageController.java
+++ boardProject/src/main/java/edu/kh/project/mypage/controller/MyPageController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.dto.Member;
@@ -101,4 +104,122 @@ public class MyPageController {
 		
 		return "redirect:info"; // myPage/info GET 방식 요청
 	}
+	
+	/** (비동기) 닉네임 중복 검사
+	 * @param input
+	 * @return 0 : 중복X / 1 : 중복O
+	 */
+	@ResponseBody // 응답 본문(ajax 코드)에 값 그대로 반환
+	@GetMapping("checkNickname")
+	public int checkNickname(@RequestParam("input") String input) {
+		return service.checkNickname(input);
+	}
+	
+	/**
+	 * 비밀번호 변경 화면 전환
+	 * @return
+	 */
+	@GetMapping("changePw") 
+	public String changePw() {
+		
+		// 접두사 : classpath:/templates/
+		// 접미사 : .html
+		return "myPage/myPage-changePw";
+	}
+	
+	/**
+	 * 비밀번호 변경 수행
+	 * @param currentPw : 현재 비밀번호
+	 * @param newPw : 변경하려는 새 비밀번호
+	 * @param loginMember : 세션에서 얻어온 로그인한 회원 정보
+	 * @param ra : 리다이렉트 시 request scope 로 데이터 전달하는 객체
+	 * @return
+	 */
+	@PostMapping("changePw")
+	public String changePw(
+			@RequestParam("currentPw") String currentPw,
+			@RequestParam("newPw") String newPw,
+			@SessionAttribute("loginMember") Member loginMember,
+			RedirectAttributes ra
+			) {
+		
+		// 서비스 호출 후 결과 반환 받기
+		int result = service.changePw(currentPw, newPw, loginMember);
+		
+		String message = null;
+		String path = null;
+		
+		// 결과에 따른 응답 제어
+		if(result > 0) {
+				message = "변경 완료ㅎ";
+				path = "info"; // 내 정보 페이지로 리다이렉트
+		}	else {
+				message = "현재 비밀번호가 일치하지 않아요~ㅋ";
+				path = "changePw"; // 비밀번호 변경 페이지로 리다이렉트
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		// 현재 컨트롤러 메서드 매핑 주소 : /myPage/changePw (POST)
+		// 리다이렉트 주소 : /myPage/info, /myPage/changePw (GET) 
+		return "redirect:" + path;
+		
+	}
+	
+	/**
+	 * 회원 탈퇴 페이지로 전환
+	 * @return
+	 */
+	@GetMapping("secession")
+	public String secession() {
+		
+		return "myPage/myPage-secession";
+	}
+	
+	/**
+	 * 회원 탈퇴 수행
+	 * @param memberPw : 입력된 비밀번호
+	 * @param loginMember : 세션에서 얻어온 로그인한 회원 정보
+	 * @param ra : 리다이렉트 시 request scope 데이터 전달
+	 * @param status : @SessionAttributes 로 관리되는 세션 데이터의 상태 제어(세션 만료)
+	 * @return
+	 */
+	@PostMapping("secession")
+	public String secession(
+			@RequestParam("memberPw") String memberPw,
+			@SessionAttribute("loginMember") Member loginMember,
+			RedirectAttributes ra,
+			SessionStatus status
+			) {
+		
+		// 서비스 호출 후 결과 반환 받기
+		int result = service.secession(memberPw, loginMember);
+		
+		String message = null;
+		String path = null;
+		
+		if(result > 0) {
+			message = "축하한다 탈퇴 성공했어 인마 ~ ㅋ";
+			path = "/"; // 메인 페이지로 리다이렉트
+			status.setComplete(); // 세션 만료 -> 로그아웃
+		} else {
+			message = "비밀번호가 틀렸잖아 인마 ~ ㅋ";
+			path = "secession"; // 탈퇴 페이지 리다이렉트
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return "redirect:" + path;
+	}
+	/* @RequestParam
+	 * - 요청 시 제출된 데이터(쿼리스트링, input)를 얻어와 매개변수에 저장하는 어노테이션
+	 * @RequestMapping 
+	 * - 요청 주소에 따라서 알맞은 컨트롤러 클래스 / 메서드에 연결하는 어노테이션
+	 * @RequestBody    
+	 * - 비동기 요청 시 body 에 담겨져 전달되는 데이터를 매개변수에 저장하는 어노테이션
+	 * @ResponseBody   
+	 * - 비동기 요청 코드(응답 본문)에 컨트롤러 반환 값을 그대로 전달하는 어노테이션
+	 */
+	
+	
 }
--- boardProject/src/main/java/edu/kh/project/mypage/mapper/MyPageMapper.java
+++ boardProject/src/main/java/edu/kh/project/mypage/mapper/MyPageMapper.java
@@ -1,17 +1,50 @@
 package edu.kh.project.mypage.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import edu.kh.project.member.dto.Member;
 
 @Mapper // 상속 받은 클래스 구현 + Bean 등록
 public interface MyPageMapper {
 
-	/**
-	 * 회원 정보 수정
-	 * @param inputMember
+	 /** 회원 정보 수정
+	  * @param inputMember
+	  * @return
+	  */
+	int updateInfo(Member inputMember);
+
+	/** 닉네임 중복 검사
+	 * @param input
+	 * @return result
+	 */
+	int checkNickname(String input);
+	
+	/** 비밀번호 수정
+	 * @param memberNo
+	 * @param encPw
 	 * @return
 	 */
-	int updateInfo(Member inputMember);
+	int changePw(
+			@Param("memberNo") int memberNo, 
+			@Param("encPw") String encPw);
+
+	/** 회원 탈퇴
+	 * @param memberPw
+	 * @param memberNo
+	 * @return result
+	 */
+	int secession(int memberNo);
+	
+	/* 마이바티스 Mapper 인터페이스 메서드 호출 시 별도의 어노테이션이 없다면
+	 * 첫 번째 매개 변수 만 mapper.xml 파일에 전달되는 parameter 로 인식된다!! 
+	 * 
+	 * [해결 방법]
+	 * 1. DTO, 컬렉션을 이용해 묶어서 전달
+	 * 2. @Param 어노테이션을 이용해 파라미터로 인식
+	 * 
+	 * @Param("key") 자료형 변수명
+	 * - SQL 중 #{key} 자리에 들어 갈 값을 지정
+	 */
 
 }
