b55cfbae2dd6a6004e78fd9f6551027e042208b8
Kangjinhyoung <a51615161@daum.net>
2024-10-16 14:40:28 +0900

    20241016_수업내용

--- boardProject/src/main/resources/static/js/board/boardList.js
+++ boardProject/src/main/resources/static/js/board/boardList.js
@@ -24,7 +24,9 @@ pageNoList?.forEach( (item, index) => {
       return;
     } 
 
+    // const -> let으로 변경
     let pathname = location.pathname; // 현재 게시판 조회 요청 주소
+
 
     // 클릭된 버튼이 <<, <, >, >> 인 경우
     // console.log(item.innerText);
@@ -51,13 +53,59 @@ pageNoList?.forEach( (item, index) => {
 
     /* 검색인 경우 pathname 변수에 뒤에 쿼리스트링 추가 */
     
+    // URLSearchParams : 쿼리스트링을 관리하는 객체
+    // - 쿼리스트링 생성, 기존 쿼리 스트링을 k:v 형태로 분할 관리
+    const params = new URLSearchParams(location.search);
 
+    const key = params.get("key"); // K가 "key"인 요소의 값
+    const query = params.get("query"); // K가 "query"인 요소의 값
+
+    if(key !== null){ // 검색인 경우
+      pathname += `&key=${key}&query=${query}`;
+    }
+
+    // 페이지 이동
+    location.href = pathname;
   });
+});
 
-})
+// ------------------------------------------------------------
+
+/* 쿼리스트링에 검색 기록이 있을 경우 화면에 
+  똑같이 선택/출력 하기 */
+
+// 즉시 실행 함수
+// - 변수명 중복 문제 해결 + 약간의 속도적 우위를 가지는 함수
+// (()=>{})()
+
+(()=>{
+
+  // 쿼리스트링 모두 얻어와 관리하는 객체
+  const params = new URLSearchParams(location.search);
+
+  const key = params.get("key");
+  const query = params.get("query");
+
+  if(key === null) return; // 검색이 아니면 함수 종료
+
+  // 검색어 화면에 출력하기
+  document.querySelector("#searchQuery").value = query;
+
+  // 검색 조건 선택하기
+  const options = document.querySelectorAll("#searchKey > option");
+
+  options.forEach( op => {
+    // op : <option> 태그
+    if(op.value === key){ // option의 valeu와 key가 같다면
+      op.selected = true; // selected 속성 추가
+      return;
+    }
+  })
+
+})();
 
 
-// ---------------------------
+// ---------------------------------------------------------
 
 /* 글쓰기 버튼 클릭 시 */
 const insertBtn = document.querySelector("#insertBtn");
--- boardProject/src/main/resources/static/js/board/boardUpdate.js
+++ boardProject/src/main/resources/static/js/board/boardUpdate.js
@@ -1,15 +1,18 @@
 // boardUpdate.html에 작성된 전역 변수들
-// const imageList = /*[[${board.imageList}]]*/ []; 
-// const previewList = document.querySelectorAll('img.preview');
+//const imageList = /*[[${board.imageList}]]*/ [];
+//const previewList = document.querySelectorAll('img.preview');
+
 
 // 기존에 존재하던 이미지의 순서(order)를 기록할 배열
-const orderList = [];
+const orderList = []; 
 
-// X 버튼이 눌러져 삭제되는 이미지의 순서(order)를 기록하는 Set
+// X버튼이 눌러져 삭제되는 이미지의 
+// 순서(order)를 기록하는 Set
 const deleteOrderList = new Set();
 // Set : 중복된 값을 저장 못하게하는 객체(Java Set 똑같음)
-// * Set을 사용하는 이유 : X 버튼이 눌러질 때 마다 order가 저장될 예정인데
-//                         중복되는 값을 저장 못하게 하기 위해서 
+// * Set을 사용하는 이유 : 
+// X 버튼이 눌러질 때 마다 order가 저장될 예정인데
+// 중복되는 값을 저장 못하게 하기 위해서
 
 // input type="file" 태그들
 const inputImageList = document.getElementsByClassName("inputImage");
@@ -19,6 +22,8 @@ const deleteImageList = document.getElementsByClassName("delete-image");
 
 // 마지막으로 선택된 파일을 저장할 배열
 const lastValidFiles = [null, null, null, null, null];
+
+
 
 /** 미리보기 함수
  * @param  file : <input type="file"> 에서 선택된 파일
@@ -48,27 +53,30 @@ const updatePreview = (file, order) => {
     return;
   }
 
+  
   // 선택된 이미지 백업
   lastValidFiles[order] = file;
 
   // JS에서 제공하는 파읽을 읽어오는 객체
   const reader = new FileReader();
 
-  // 파일을 읽어 오는데 DataURL 형식으로 읽어옴
-  // DataURL: 파일 전체 데이터가 브라우저가 해석할 수 있는 긴 주소형태 문자열로 변환
+  // 파일을 읽어 오는데
+  // DataURL 형식으로 읽어옴
+  // DataURL: 파일 전체 데이터가 브라우저가 해석할 수 있는
+  //          긴 주소형태 문자열로 변환
   reader.readAsDataURL(file);
 
   reader.addEventListener("load", e => {
     previewList[order].src = e.target.result;
     // e.target.result == 파일이 변환된 주소 형태 문자열
 
-    // 이미지가 성공적으로 읽어진 경우 deleteOrderList에서 해당 이미지 순서를 삭제
-    // -> 왜? 이전에 X 버튼을 눌러 삭제 기록이 있을수도 있기 때문에
+    // 이미지가 성공적으로 읽어진 경우
+    // deleteOrderList에서 해당 이미지 순서를 삭제
+    // -> 왜?? 이전에 X 버튼을 눌러 삭제 기록이 있을 수도 있기 때문에
     deleteOrderList.delete(order);
-  });
-};
+  })
+}
 
-// ----------------------------------------------------------------
 
 /* input태그, x버튼에 이벤트 리스너 추가 */
 for (let i = 0; i < inputImageList.length; i++) {
@@ -102,6 +110,8 @@ for (let i = 0; i < inputImageList.length; i++) {
     updatePreview(file, i);
   })
 
+
+
   /* X 버튼 클릭 시 미리보기, 선택된 파일 삭제 */
   deleteImageList[i].addEventListener("click", () => {
 
@@ -109,16 +119,19 @@ for (let i = 0; i < inputImageList.length; i++) {
     inputImageList[i].value = ""; // 선택된 파일 삭제
     lastValidFiles[i]       = null; // 백업 파일 삭제
 
-    // 기존에 존재하던 이미지가 있는 상태에서 X 버튼이 눌러 졌을 때
-    // -> 기존에 이미지가 있었는데 i번째 이미지 X 버튼 눌러서 삭제함 --> DELETE 수행
-    if(orderList.includes(i)) {
-      deleteOrderList.add(i);
+    // 기존에 존재하던 이미지가 있는 상태에서
+    // X 버튼이 눌러 졌을 때
+    // --> 기존에 이미지가 있었는데 
+    //     i번째 이미지 X버튼 눌러서 삭제함 --> DELETE 수행
+    if(orderList.includes(i)){
+      deleteOrderList.add(i); 
     }
 
-  });
+  })
+
 } // for end
 
-// ----------------------------------------------------------------
+// -----------------------------------------------------------------------
 
 /* 제목, 내용 미작성 시 제출 불가 */
 const form = document.querySelector("#boardUpdateFrm");
@@ -142,22 +155,20 @@ form.addEventListener("submit", e => {
     return;
   }
 
-  // 제출 전에 form 태그 마지막 자식으로 input 추가 한 후 제출
-  // -> 해당 input에는 삭제된 이미지 순서(deleteOrderList)를 담음
+
+  // 제출 전에 form 태그 마지막 자식으로
+  // input 추가 한 후 제출
+  // -> 해당 input에는
+  //   삭제된 이미지 순서(deleteOrderList)를 추가
+
   const input = document.createElement("input");
 
-  // Array.from : Set -> Array로 변환
-  // 배열.toString() : [1, 2, 3] --> "1, 2, 3"으로 변환
-  input.value = Array.from(deleteOrderList);
+  // Array.from() : Set -> Array로 변환
+  // 배열.toString() : [1,2,3] --> "1,2,3" 변환
+  input.value = Array.from(deleteOrderList).toString();
 
   input.name = "deleteOrderList";
   input.type = "hidden";
 
   form.append(input); // 자식으로 input 추가
-
-});
-
-
-
-
-
+})
